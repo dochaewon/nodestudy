22afc595e22300784087b2c7acc733e96460b57a
winnie <ehcodnjs123@gmail.com>
2017-09-28 13:51:00 +0900

    .....

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
-var express = require('express');
-//설티된 module은 require명령어를 통해 불러 올 수 있고, express module을 ㄷexpress 라는 이름의 변수로 저장
-var app = express();
-//express에()가 붙는거으로 봐서 express module은 함수라는것을 알수있다. 모든module들이 함수는아님
-//express module은 그냥함수도 아니고 생성자임
-app.get('/', function (req,res) {
-  res.send('항하이');
-}); //서버의 root에 get요청이 왔을때 '항하이'라는 신호를 클라이엉ㄴ트에 보내라는 명령어
-//app.get은 eventListener이고 두개의 argument를 받는데,첫번째는 조건,두번쨰는 반응함수임.
+var express  = require('express');
+var app      = express();
+var path     = require('path');
+var mongoose = require('mongoose');
+var session  = require('express-session');
+var flash    = require('connect-flash');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
 
-app.listen(4000,function() {
-  console.log('서버온@');
-});//app.listen도 두개의 argument를 받는데,첫번째는 port번호이고, 두번째는 함수임.4000번포트를 통해 들어오는 신호를 감지하여 반응하라는명령.
-//두번째함수는 옵션으로 꼭필요한 것은 아니지만 콘솔창에 뭐가 어떨게 되고 있는지 알려주기 위해 사용
+// database
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once("open",function () {
+  console.log("DB connected!");
+});
+db.on("error",function (err) {
+  console.log("DB ERROR :", err);
+});
+
+// view engine
+app.set("view engine", 'ejs');
+
+// middlewares
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.use(flash());
+app.use(session({secret:'MySecret'}));
+
+// passport
+var passport = require('./config/passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+// routes
+app.use('/', require('./routes/home'));
+app.use('/users', require('./routes/users'));
+app.use('/posts', require('./routes/posts'));
+
+// start server
+app.listen(3000, function(){
+  console.log('Server On!');
+});
